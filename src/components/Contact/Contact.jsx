da647ecc39e4df4cad8a2c4ab7c12df61659eefe
gourjoy <goursaha307@gmail.com>
2023-07-05 13:15:15 +0600

    bug fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -24,16 +24,16 @@ const Contact = () => {
          <SectionTitle title="let's talk" />
          <form data-aos="fade-right" ref={form} onSubmit={sendEmail} className='max-w-2xl mx-auto'>
             {/* name */}
-            <div data-aos="flip-up" data-aos-delay="200"  className="form-control w-full block">
+            <div data-aos="flip-up" data-aos-delay="200" className="form-control w-full block">
                <label className="label">
                   <span className="label-text">What is your name?</span>
                </label>
-               <input type="text" name='name' placeholder="Name" className="input input-bordered w-full dark:bg-slate-800 bg-gray-200" />
+               <input type="text" name='name' placeholder="Name" className="input input-bordered w-full dark:bg-slate-800 bg-gray-200" required />
             </div>
             {/* Whatsapp */}
             <div data-aos="flip-down" data-aos-delay="300" className="form-control w-full block">
                <label className="label">
-                  <span className="label-text">Your Whatsapp Number</span>
+                  <span className="label-text">Your Whatsapp Number (optional)</span>
                </label>
                <input type="tel" name='number' placeholder="Whatsapp" className="input input-bordered w-full dark:bg-slate-800 bg-gray-200" />
             </div>
@@ -42,14 +42,14 @@ const Contact = () => {
                <label className="label">
                   <span className="label-text">Your Email</span>
                </label>
-               <input type="email" name='email' placeholder="Email" className="input input-bordered w-full dark:bg-slate-800 bg-gray-200" />
+               <input type="email" name='email' placeholder="Email" className="input input-bordered w-full dark:bg-slate-800 bg-gray-200" required />
             </div>
             {/* message */}
             <div data-aos="flip-down" data-aos-delay="500" className="form-control w-full block">
                <label className="label">
                   <span className="label-text">Your Message</span>
                </label>
-               <textarea name='message' className="textarea w-full dark:bg-slate-800 bg-gray-200" placeholder="Message"></textarea>
+               <textarea name='message' className="textarea w-full dark:bg-slate-800 bg-gray-200" placeholder="Message" required></textarea>
             </div>
             <button data-aos="zoom-in" data-aos-delay="600" type='submit' className="button-send w-full mt-3"> send
             </button>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
    return (
       <footer className="footer footer-center p-10 border-t">
          <div>
-         <Lottie className="w-56" animationData={man}/>
+            <Lottie className="w-56" animationData={man} />
             <p className="font-bold">
                <span className="text-xl">Gour Chandra Saha</span><br />Junior Front-End Developer
             </p>
@@ -17,22 +17,22 @@ const Footer = () => {
          <div>
             <div className='mb-4 space-x-3'>
                {/* facebook */}
-               <button data-aos="zoom-in" data-aos-delay="400">
+               <button data-aos="zoom-in" data-aos-anchor-placement="bottom-bottom" data-aos-delay="400">
                   <a target='_blank' rel='noreferrer' href="https://web.facebook.com/GSJoy.24"
                   ><FaFacebook size={30} className='border-black dark:border-white rounded-full active:scale-90 ease-in-out duration-200 text-blue-500 dark:text-white' /></a>
                </button>
                {/* linkedin */}
-               <button data-aos="zoom-in" data-aos-delay="500">
+               <button data-aos="zoom-in" data-aos-delay="500" data-aos-anchor-placement="bottom-bottom">
                   <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/gsjoy24/"
                   ><FaLinkedin size={30} className='border-black dark:border-white active:scale-90 ease-in-out duration-200 text-[#0072b1] dark:text-white' /></a>
                </button>
                {/* gmail */}
-               <button data-aos="zoom-in" data-aos-delay="600">
+               <button data-aos="zoom-in" data-aos-delay="600" data-aos-anchor-placement="bottom-bottom">
                   <a target='_blank' rel='noreferrer' href='mailto:gour.joy24@gmail.com'
                   ><SiGmail size={30} className=' border-black dark:border-white  active:scale-90 ease-in-out duration-200 text-[#BD150F] dark:text-white' /></a>
                </button>
                {/* github */}
-               <button data-aos="zoom-in" data-aos-delay="700">
+               <button data-aos="zoom-in" data-aos-delay="700" data-aos-anchor-placement="bottom-bottom">
                   <a target='_blank' rel='noreferrer' href="https://github.com/gsjoy24/"
                   ><FaGithub size={30} className='border-black dark:border-white active:scale-90 ease-in-out duration-200 text-[#171515] dark:text-white' /></a>
                </button>
